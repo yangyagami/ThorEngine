08051f8f2704c670527b6031083ee218007a7aab
Siyu Yang <a651685099@163.com>
2023-10-11 09:07:33 +0800

    edit xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ add_rules("mode.debug", "mode.release")
 
 target("ThorTest")
 	set_kind("binary")
-	add_includedirs("engine", "engine/core", "platform/linux", "3rdparty/glad/include")
-	add_files("test/*.cpp", "engine/*.cpp", "engine/core/*.cpp", "platform/linux/*.cpp", "3rdparty/glad/src/*.c")
+	add_includedirs("engine", "engine/core", "platform/linux/GLFWwindow", "3rdparty/glad/include")
+	add_files("test/*.cpp", "engine/*.cpp", "engine/core/*.cpp", "platform/linux/GLFWwindow/*.cpp", "3rdparty/glad/src/*.c")
 
 	add_packages("spdlog", "glfw")
